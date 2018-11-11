24bf121afc2b6f8b30631ae942818047b3eacc47
RahmatWahyudi57 <44788959+RahmatWahyudi57@users.noreply.github.com>
2018-11-11 20:06:07 +0700

    Add files via upload

---
+++ TugasDasPro/Makanan.java
@@ -0,0 +1,13 @@
+class Nasi{
+String Bahan1 = "Beras";
+String Bahan2 = "Air";
+}
+
+public class Makanan{
+public static void main(String[] args){
+Nasi obj_nasi = new Nasi();
+
+System.out.println("Bahan1 Nasi : "+obj_nasi.Bahan1);
+System.out.println("Bahan2 Nasi : "+obj_nasi.Bahan2);
+}
+}
